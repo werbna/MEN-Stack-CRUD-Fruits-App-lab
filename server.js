9c53a88f879a9c0b791684df27ddc2ce91899466
werbnah <andrewchau1993@gmail.com>
2024-07-25 13:49:00 -0700

    added form

--- server.js
+++ server.js
@@ -15,6 +15,10 @@ mongoose.connection.on("connected", () => {
 // Import the Fruit model
 const Fruit = require("./models/fruit.js");
 
+// middleware
+app.use(express.urlencoded({ extended: false }));
+
+
 app.listen(3000, () => {
   console.log("Listening on port 3000");
 });
@@ -24,7 +28,23 @@ app.get('/', (req,res) => {
   res.render('index.ejs');
 })
 
+app.get('fruits', (req,res) => {
+  res.render('/fruits')
+})
+
+
 // GET /fruits/new
 app.get("/fruits/new", (req, res) => {
   res.render("fruits/new.ejs");
+});
+
+// POST /fruits
+app.post("/fruits", async (req, res) => {
+  if (req.body.isReadyToEat === "on") {
+    req.body.isReadyToEat = true;
+  } else {
+    req.body.isReadyToEat = false;
+  }
+  await Fruit.create(req.body);
+  res.redirect("/fruits/new");
 });
