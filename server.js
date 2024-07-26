a48e32e886950ec580e3c97d635b860297dac517
werbnah <andrewchau1993@gmail.com>
2024-07-26 10:10:15 -0700

    add links to pages for navigation and add show functionality

--- server.js
+++ server.js
@@ -4,12 +4,19 @@ const dotenv = require("dotenv");
 dotenv.config();
 const express = require("express");
 const mongoose = require("mongoose");
-
 const app = express();
+const methodOverride = require('method-override');
+const morgan = require('morgan');
+const path = require('path');
 
+mongoose.connect(process.env.MONGODB_URI);
 // log connection status to terminal on start
 mongoose.connection.on("connected", () => {
-  console.log(`Connected to MongoDB ${mongoose.MONGODB_URI}.`);
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
 });
 
 // Import the Fruit model
@@ -17,25 +24,32 @@ const Fruit = require("./models/fruit.js");
 
 // middleware
 app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+// app.use(morgan('dev'));
 
-
-app.listen(3000, () => {
-  console.log("Listening on port 3000");
-});
+// new code below this line
+app.use(express.static(path.join(__dirname, "public")));
 
 // GET 
 app.get('/', (req,res) => {
   res.render('index.ejs');
 })
-
-app.get('fruits', (req,res) => {
-  res.render('/fruits')
-})
-
+// GET /fruits
+app.get("/fruits", async (req, res) => {
+  const allFruits = await Fruit.find();
+  console.log(allFruits); // log the fruits!
+  res.render('fruits/index.ejs', { fruits: allFruits });
+});
 
 // GET /fruits/new
 app.get("/fruits/new", (req, res) => {
   res.render("fruits/new.ejs");
+});
+
+// GET /fruits:fruitId
+app.get("/fruits/:fruitId", async (req, res) => {
+  const foundFruit = await Fruit.findById(req.params.fruitId);
+  res.render("fruits/show.ejs", { fruit: foundFruit });
 });
 
 // POST /fruits
