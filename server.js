e2d7f0436a08c577a343fafa751639092d1832ac
werbnah <andrewchau1993@gmail.com>
2024-07-26 11:15:00 -0700

    installed morgan, method-override, completed delete and edit functionality

--- server.js
+++ server.js
@@ -9,26 +9,23 @@ const methodOverride = require('method-override');
 const morgan = require('morgan');
 const path = require('path');
 
+// middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method")); // new
+app.use(morgan("dev")); //new
+
+// new code below this line
+app.use(express.static(path.join(__dirname, "public")));
+
+
 mongoose.connect(process.env.MONGODB_URI);
 // log connection status to terminal on start
 mongoose.connection.on("connected", () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000");
-});
-
 // Import the Fruit model
 const Fruit = require("./models/fruit.js");
-
-// middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(methodOverride("_method"));
-// app.use(morgan('dev'));
-
-// new code below this line
-app.use(express.static(path.join(__dirname, "public")));
 
 // GET 
 app.get('/', (req,res) => {
@@ -61,4 +58,37 @@ app.post("/fruits", async (req, res) => {
   }
   await Fruit.create(req.body);
   res.redirect("/fruits/new");
+});
+
+// Delete /fruit/fruitId
+app.delete("/fruits/:fruitId", async (req, res) => {
+  await Fruit.findByIdAndDelete(req.params.fruitId);
+  res.redirect("/fruits");
+});
+
+// PUT /fruit/fruitId/edit
+app.get("/fruits/:fruitId/edit", async (req, res) => {
+  const foundFruit = await Fruit.findById(req.params.fruitId);
+  res.render("fruits/edit.ejs", {
+    fruit: foundFruit,
+  });
+});
+
+app.put("/fruits/:fruitId", async (req, res) => {
+  // Handle the 'isReadyToEat' checkbox data
+  if (req.body.isReadyToEat === "on") {
+    req.body.isReadyToEat = true;
+  } else {
+    req.body.isReadyToEat = false;
+  }
+  
+  // Update the fruit in the database
+  await Fruit.findByIdAndUpdate(req.params.fruitId, req.body);
+
+  // Redirect to the fruit's show page to see the updates
+  res.redirect(`/fruits/${req.params.fruitId}`);
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
 });
